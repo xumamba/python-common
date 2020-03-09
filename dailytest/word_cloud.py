feef80b52a9d6b126c52e44d8173095d5bf17cbb
xulp <Xu_Liangpan@163.com>
2020-03-09 10:56:06 +0800

    feat:word cloud generator

--- dailytest/word_cloud.py
+++ dailytest/word_cloud.py
@@ -15,5 +15,5 @@ translation plugins
 
 cut_list = jieba.lcut(s)  # 分词
 new_str = ' '.join(cut_list)  # 用空格从拼接成一个字符串
-word_cloud = WordCloud(font_path='msyh.ttc').generate(new_str)  # 生成词云对象
-word_cloud.to_file('test.png')  # 保存到图片
+word_cloud = WordCloud().generate(new_str)  # 生成词云对象
+word_cloud.to_file('word_cloud.png')  # 保存到图片
