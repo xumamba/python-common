3a9343e8732dc847489784aada832757f82378d0
xulp <Xu_Liangpan@163.com>
2020-04-01 22:30:55 +0800

    feat:error exception

---
+++ liberary/errros/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/27 上午9:14
+# @Author  : serendipity-xp
+# @FileName: __init__.py
+# @Software: PyCharm
+# @GitHub  ：https://github.com/serendipity-xp
+"""
+自定义错误类
+"""
+
+
+class ServerInnerError(Exception):
+    """服务内部错误"""
+
