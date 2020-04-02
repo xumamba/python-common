ab6994d2e8860e4bb8cc762b135f4d950060be24
徐梁攀 <1057262993@qq.com>
2020-04-02 17:56:45 +0800

    feat: format

---
+++ dailytest/str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @DateTime: 2020/4/2 15:15
+
+# print '({0}-{1})'.format(*[u'\u4e00', u'\u9fa5'])
+print ('hello {name1}  i am {name2}'.format(name1='Kevin', name2='Tom'))
+
+args = ['aa']
+kwargs = {'name1': 'tom', 'name2': 'jerry'}
+print 'hello {name1} {} i am {name2}'.format(*args, **kwargs)
+
+
+def add(a, b):
+    return a + b
+
+
+data = [1, 2]
+print add(*data)
+data = {'a': 3, 'b': 4}
+print add(**data)
